2fa37592fb8195d5d9521b36806af3218e3b6fac
SantiagoAHS <lionelsanti2018@gmail.com>
2025-02-20 19:35:36 -0600

    04 Components for properies

---
+++ app/components/properties/PropertyList.tsx
@@ -0,0 +1,12 @@
+import PRopertyListItem from "./PropertyListItem";
+
+const PropertyList =()=>{
+    return(
+        <>
+            <PRopertyListItem/>
+            <PRopertyListItem/>
+            <PRopertyListItem/>
+        </>
+    )
+}
+export default PropertyList;
