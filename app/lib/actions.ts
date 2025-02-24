3cb9a6a1f6700e29b673fb78f40294ea5a7de866
SantiagoAHS <lionelsanti2018@gmail.com>
2025-02-24 17:33:37 -0600

    15 Add properties from nextjs create modal , api endpoint , send data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,3 +36,9 @@ export async function getUserId() {
     return userId ? userId : null
 }
 
+export async function getAccessToken() {
+    let accessToken = (await cookies()).get('session_access_token')?.value;
+
+    return accessToken;
+}
+
